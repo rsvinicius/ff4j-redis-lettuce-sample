1af3695efcc4ee20b6dffd95047a359df41772c3
Vinicius R <vrodrigues.code@gmail.com>
2022-08-09 19:05:44 -0300

    spring initializr

---
+++ src/main/kotlin/com/example/ff4jredissample/Ff4jRedisSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ff4jredissample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ff4jRedisSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<Ff4jRedisSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ff4jredissample/Ff4jRedisSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ff4jredissample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ff4jRedisSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
